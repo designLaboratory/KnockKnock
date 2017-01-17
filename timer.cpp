4af69014757ccc7b3c86a3cdd5b74ab45649ac02
ABarnasFLY <Adrian95cc@gmail.com>
2017-01-17 01:07:04 +0100

    Fix interupts handling in c++

--- timer.cpp
+++ timer.cpp
@@ -1,8 +1,34 @@
 #include "timer.h"
 
+extern "C" {
+  void SysTick_Handler(void)
+  {
+		if(Timer::isStarted())
+			Timer::tick(); 
+  }
+}
+
+void Timer::initSysTick()
+{
+	SystemCoreClockUpdate();
+	SysTick_Config(SystemCoreClock/1000);
+	m_sysTickInitialized = 1;
+}
+bool Timer::isStarted()
+{
+	return m_started;
+}
+
+bool Timer::m_started = 0;
 unsigned int Timer::m_value = 0;
 unsigned int Timer::m_compare = 0;
 bool Timer::m_timeout = 0;
+bool Timer::m_sysTickInitialized = 0;
+void Timer::tick()
+{
+	m_value++;
+	if(m_value > m_compare) m_timeout = true;
+}
 void Timer::wait(unsigned int milisecondes)
 {
 	setCompare(milisecondes);
@@ -12,11 +38,13 @@ void Timer::wait(unsigned int milisecondes)
 
 void Timer::start()
 {
+	if(m_sysTickInitialized)
+	{
 	m_timeout = false;
 	m_value = 0;
-	SysTick_Config(SystemCoreClock / 1000);
-	NVIC_ClearPendingIRQ(SysTick_IRQn);
-	NVIC_EnableIRQ(SysTick_IRQn);	
+	m_started = 1;
+	}
+	else m_timeout = 1;
 }
 
 void Timer::setCompare(unsigned int milisecondes)
@@ -29,12 +57,4 @@ bool Timer::timeout()
 	return m_timeout;
 }
 
-void SysTick_Handler()
-{
-	Timer::m_value++;
-	if (Timer::m_value > Timer::m_compare)
-	{
-		Timer::m_timeout = true;
-		NVIC_DisableIRQ(SysTick_IRQn);
-	}
-}
+
--- timer.h
+++ timer.h
@@ -3,9 +3,15 @@
 class Timer
 {
 	public:
+		static void initSysTick();
+		static bool m_started;
 		static bool m_timeout;
 		static unsigned int m_compare;
 		static unsigned int m_value;
+		static bool m_sysTickInitialized;
+	public:
+		static bool isStarted();
+		static void tick();
 		static void wait(unsigned int miliseconds);
 		static void start();
 		static bool timeout();
